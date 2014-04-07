6b89b022bd198d02ff20e01adb381ab9ee1672ad
Matthew Dahl <dahl.matthew@gmail.com>
2014-04-06 22:45:59 -0700

    improved commenting and cleaned up code

--- src/concat.c
+++ src/concat.c
@@ -3,23 +3,57 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Make buffer size 8KB's
+// Size of buffer used when reading files
 #define BUFFER_SIZE 8192
-#define DEBUG 1
 
+/**
+ * Free an array of strings from memory
+ *
+ * @param[in] array arry of strings to free
+ * @param[in] count number of strings in array
+ */
 void freeStringArray(char **array, int count);
+
+/**
+ * Get a file handle for an output stream based on a file path
+ *
+ * @param[in] outputPath
+ * @param[out]outputStream
+ * @return error -1 if error otherwise 0
+ */
 char getOutStream(char *outputPath, FILE **outStream);
+
+/**
+ * Parse command line arguments
+ *
+ * @param[in] argc argument count
+ * @param[in] argv argument values
+ * @param[out] inputPaths array of input file paths
+ * @param[out] inputCount number of input file paths
+ * @param[out] outputPath file path to write output to
+ * @return error -1 if error otherwise 0
+ */
 char parseArgs(int argc, char *argv[], char **inputPaths, int *inputCount, char **outputPath);
+
+/**
+ * Write concatenation of input files to an output file
+ *
+ * @param[in] inputPaths
+ * @param[in] inputCount
+ * @param[in] outStream
+ */
 void writeFiles(char **inputPaths, int inputCount, FILE *outStream);
 
+/**
+ * Entry point of program
+ *
+ * @param[in] argc argument count
+ * @param[in] argv argument values
+ */
 int main(int argc, char *argv[]) {
   int inputCount;
   char **inputPaths = malloc(sizeof(char*) * argc);
   char *outputPath = NULL;
-
-  #ifdef DEBUG
-    printf("Parse args...\n");
-  #endif
 
   if (parseArgs(argc, argv, inputPaths, &inputCount, &outputPath) == (char)-1) {
     freeStringArray(inputPaths, inputCount);
@@ -31,44 +65,38 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  #ifdef DEBUG
-    printf("Get output stream...\n");
-  #endif
-
   FILE *outStream;
   char result = getOutStream(outputPath, &outStream);
 
-  // We no longer need the output path so free it from memory if it was set
+  // If output path was set then free it since it is no longer needed
   if (outputPath != NULL) {
     free(outputPath);
   }
 
-  // If it failed to open output file for writing to
+  // If getOutStream() failed to open output file for writing to
   if (result == (signed char)-1) {
     freeStringArray(inputPaths, inputCount);
     exit(EXIT_FAILURE);
   }
 
-  #ifdef DEBUG
-    printf("Write files...\n");
-  #endif
-
   writeFiles(inputPaths, inputCount, outStream);
 
+  // If output stream is a file, make sure we close our file handle
   if (outStream != stdout) {
     fclose(outStream);
   }
 
   freeStringArray(inputPaths, inputCount);
-
   exit(EXIT_SUCCESS);
 }
 
 void freeStringArray(char **array, int count) {
+  // Free each string in array
   for (int i = 0; i < count; i++) {
     free(array[i]);
   }
 
+  // Free array itself
   free(array);
 }
 
@@ -102,20 +130,17 @@ char parseArgs(int argc, char *argv[], char **inputPaths, int *inputCount, char 
 
   // Iterate arguments (skip first argument since it is this program)
   for (int i = 1; i < argc; i++) {
-    // Check if argument starts with a '-' indicating it is an option
-    if (argv[i][0] == '-') {
-      if (argv[i][1] == 'o') {
-        if (++i >= argc) {
-          fprintf(stderr, "You must provide an output path when using -o command line option.\n");
-          return -1;
-        }
-        int length = strlen(argv[i]);
-        *outputPath = malloc(sizeof(char) * (length + 1));
-        strcpy(*outputPath, argv[i]);
-      } else {
-        fprintf(stderr, "%s is not a valid command line option.\n", argv[i]);
+    if (strcmp(argv[i], "-o") == 0) {
+      if (++i >= argc) {
+        fprintf(stderr, "You must provide an output path when using -o command line option.\n");
         return -1;
       }
+      int length = strlen(argv[i]);
+      *outputPath = malloc(sizeof(char) * (length + 1));
+      strcpy(*outputPath, argv[i]);
+    } else if (argv[i][0] == '-') {
+      fprintf(stderr, "%s is not a valid command line option.\n", argv[i]);
+      return -1;
     } else {
       inputPaths[*inputCount] = malloc(sizeof(char) * (strlen(argv[i]) + 1));
       strcpy(inputPaths[*inputCount], argv[i]);
